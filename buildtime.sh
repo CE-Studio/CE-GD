c897cae17092b9165834c0cb8d40086fe165c996
clarence112 <clarencepacelli@gmail.com>
2023-11-24 14:29:03 -0500

    branding

--- buildtime.sh
+++ buildtime.sh
@@ -1,1 +1,1 @@
-scons -j8 platform=linuxbsd target=editor use_lto=yes && scons -j8 platform=windows target=editor use_lto=yes
+scons -j8 platform=linuxbsd target=editor use_lto=yes precision=double && scons -j8 platform=windows target=editor use_lto=yes precision=double && scons platform=web dlink_enabled=yes target=template_debug precision=double
