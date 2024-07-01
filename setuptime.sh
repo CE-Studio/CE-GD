6a1f6be1c7b541eb976fdf6934239b4171018e85
clarence112 <clarencepacelli@gmail.com>
2024-07-01 00:39:02 -0400

    fis some things

--- setuptime.sh
+++ setuptime.sh
@@ -1,5 +1,5 @@
-sudo apt-get update
-sudo apt-get install -y build-essential scons pkg-config libx11-dev libxcursor-dev libxinerama-dev libgl1-mesa-dev libglu1-mesa-dev libasound2-dev libpulse-dev libudev-dev libxi-dev libxrandr-dev
+sudo apt update
+sudo apt install -y build-essential scons pkg-config libx11-dev libxcursor-dev libxinerama-dev libgl1-mesa-dev libglu1-mesa-dev libasound2-dev libpulse-dev libudev-dev libxi-dev libxrandr-dev mingw-w64
 cd ../
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
