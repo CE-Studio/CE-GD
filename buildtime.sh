d4f46fb53a8ab813452bd4a5f6c025810c6b290a
clarence112 <clarencepacelli@gmail.com>
2025-04-27 11:43:03 -0400

    cs

--- buildtime.sh
+++ buildtime.sh
@@ -6,4 +6,6 @@ export ANDROID_HOME=$CUR/android
 export PATH="$PATH:$ANDROID_HOME/cmdline-tools/bin"
 export JAVA_HOME=/usr/lib/jvm/java-1.17.0-openjdk-amd64
 cd CE-GD
-scons -j11 platform=linuxbsd target=editor use_lto=yes precision=double && scons -j11 platform=windows target=editor use_lto=yes precision=double && scons -j11 platform=web dlink_enabled=yes target=template_debug precision=double && scons -j11 platform=android arch=arm64 production=yes target=editor precision=double generate_apk=yes
+scons -j11 platform=linuxbsd target=editor use_lto=yes precision=double && scons -j11 platform=windows target=editor use_lto=yes precision=double && scons -j11 platform=web dlink_enabled=yes target=template_debug precision=double && scons -j11 platform=android arch=arm64 production=yes target=editor precision=double generate_apk=yes && scons -j11 platform=linuxbsd target=editor use_lto=yes precision=double module_mono_enabled=yes
+./bin/godot.linuxbsd.editor.double.x86_64.mono --headless --generate-mono-glue ./modules/mono/glue/
+./modules/mono/build_scripts/build_assemblies.py --godot-output-dir ./bin --push-nupkgs-local ~/MyLocalNugetSource --precision=double
